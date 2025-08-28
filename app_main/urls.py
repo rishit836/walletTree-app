7696c86976b79668f480e302e11c8bfe87f70a15
Rishit Sharma <rishitshar36@gmail.com>
2025-08-28 14:11:54 +0530

    bug fixes

--- app_main/urls.py
+++ app_main/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 app_name = 'main'
 urlpatterns = [
+<<<<<<< HEAD
     path('',views.home,name="home")
+=======
+    path('',views.home,name="home"),
+    path('login',views.login_view, name="login"),
+    path('signup',views.signup_view,name="signup"),
+    path('logout',views.logout_view,name="logout")
+>>>>>>> 290a52e (decluttered signup and login page and now backend works)
 ]
